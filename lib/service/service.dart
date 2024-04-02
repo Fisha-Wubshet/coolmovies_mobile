47b26bce8cd09c17266c9e4f0e7dff6510371ca5
Fisha Wubshet <fisha.wubshet@gmail.com>
2024-04-02 11:55:08 +0300

    Implemented Service MovieService for fetching and add reviews

---
+++ lib/service/service.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+import '../model/model.dart';
+import 'querys.dart';
+
+class MovieService {
+  static final HttpLink _httpLink = HttpLink( 
+    Platform.isAndroid ? 'http://10.0.2.2:5001/graphql' : 'http://localhost:5001/graphql',
+    );
+  static final ValueNotifier<GraphQLClient> _client = ValueNotifier(
+    GraphQLClient(link: _httpLink, cache: GraphQLCache(store: InMemoryStore())),
+  );
+  static GraphQLClient get client => _client.value;
+
+// fetch movies service
+  static Future<List<Movie>> fetchMovies() async {
+    final query = gql(fetchMoviesQuery);
+    final result = await client.query(QueryOptions(document: query));
+    if (result.hasException) {
+      throw Exception(result.exception);
+    }
+    final data = result.data!['allMovies']['nodes'] as List;
+    return data.map((movie) => Movie.fromJson(movie)).toList();
+  }
+
+// add review service
+  static Future<List<Movie>> addReview(movieId,userReviewerId,title,rating,body) async {
+    final query = gql(addReviewQuery(movieId,userReviewerId,title,rating,body));
+    final result = await client.query(QueryOptions(document: query));
+    if (result.hasException) {
+      throw Exception(result.exception);
+    }
+    final data = result.data!['createMovieReview']['query']['allMovies']['nodes']  as List ;
+    return data.map((review) => Movie.fromJson(review)).toList();
+  }
+
+}
