d3cfb9f5656eefeefce1cb1ce053b645cd0f2826
Fisha Wubshet <fisha.wubshet@gmail.com>
2024-04-02 12:56:11 +0300

    utils

---
+++ lib/utils/Utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'brand_color.dart';
+final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+  onPrimary: Colors.white,
+  primary:  primaryColor,
+  padding: const EdgeInsets.symmetric(horizontal: 16),
+  elevation: 0.0
+);
---
+++ lib/utils/brand_color.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+const primaryColor = Color(0xff42A823);
+const brandBackgroundColor= Color(0xFFF7FAF5);
